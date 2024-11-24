38a550a48502e759b446b97aaeef9038f8810fb0
YoloNinjaNN <muchamedshaev@gmail.com>
2024-11-24 12:48:49 +0200

    fix

---
+++ 29 JS/js/main.js
@@ -0,0 +1,12 @@
+const userName = promt("Enter your name")
+alert("Hello, " + userName + "!")
+    alert(`Hello, ${userName}`)
+
+const curremtYear = 2024
+const birthYear = promt("Enter your birth year")
+const userAge = currentYear - birthYear
+alert (`Your age is: ${userAge} years`)
+
+const width = promt ("Enter square side width")
+const perim = width * 4
+alert("Perimetr is " + perim)
